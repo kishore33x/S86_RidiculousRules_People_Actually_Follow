8266a0230977d7fe04489f9566e80e9888db905e
kishore33x <redranger637@gmail.com>
2025-02-12 14:14:17 +0530

    updated files

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Server is running! Welcome to the API.');
+});
 
 app.get('/ping', (req, res) => {
     res.json({ message: 'pong' });
